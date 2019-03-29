8ddd4fbb768261d7773b938287ed691c4c236b72
pifei <809042368@qq.com>
2019-03-29 15:07:51 +0800

    更改了相关配置文件信息

--- 6-day-Spring cloud config/cloud-server/src/main/java/com/Server/CloudServerApplication.java
+++ 6-day-Spring cloud config/cloud-server/src/main/java/com/Server/CloudServerApplication.java
@@ -2,10 +2,11 @@ package com.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
 @SpringBootApplication
-@EnableEurekaServer
 public class CloudServerApplication {
 
 	public static void main(String[] args) {
